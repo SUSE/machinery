17c3a164c46057687cbc36a29765b84bc9d1f791
Tim Hardeck <thardeck@suse.de>
2014-10-09 16:21:38 +0200

    Use scope_extracted? instead of file_store

--- lib/analyze_config_file_diffs_task.rb
+++ lib/analyze_config_file_diffs_task.rb
@@ -18,7 +18,7 @@
 class AnalyzeConfigFileDiffsTask
   def analyze(description)
     description.assert_scopes("repositories", "config_files")
-    if !description.file_store("config_files")
+    if !description.scope_extracted?("config_files")
       raise Machinery::Errors::SystemDescriptionError.new(
         "Missing extracted config files.\n" \
         "You can extract them using 'machinery inspect -s config-files -x'."
