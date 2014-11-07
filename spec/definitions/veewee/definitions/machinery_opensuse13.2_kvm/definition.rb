50d7dd5ee1c439c3286bfdaea10e8cd6c1fe76c8
chuller@suse.de <chuller@novell.com>
2014-11-07 17:42:03 +0100

    Add veewee box definitions and Vagrantfile entries for 13.2.
    - Add machinery_opensuse13.2_kvm veewee definition files
    - Add machinery_opensuse13.2_kvm veewee definition files
    - Add entry for opensuse132_kvm to Vagrantfile
    - Add entry for machinery_opensuse13.2_kvm to Vagrantfile

---
+++ spec/definitions/veewee/definitions/machinery_opensuse13.2_kvm/definition.rb
@@ -0,0 +1,42 @@
+Veewee::Definition.declare(
+  cpu_count: "2",
+  memory_size: "384",
+  disk_size: "20280",
+  disk_format: "VDI",
+  hostiocache: "off",
+  os_type_id: "OpenSUSE_64",
+  iso_file: "openSUSE-13.2-DVD-x86_64.iso",
+  iso_src: "http://download.opensuse.org/distribution/13.2/iso/openSUSE-13.2-DVD-x86_64.iso",
+  iso_md5: "350b8cb014a4e342cc9a7cc9df891b99",
+  iso_download_timeout: "1000",
+  boot_wait: "10",
+  boot_cmd_sequence: [
+    "<Esc><Enter>",
+    "linux",
+    " netdevice=eth0",
+    " netsetup=dhcp",
+    " instmode=dvd",
+    " textmode=1",
+    " autoyast=http://%IP%:8888/autoinst.xml",
+    "<Enter>"
+   ],
+  ssh_login_timeout: "10000",
+  ssh_user: "vagrant",
+  ssh_password: "vagrant",
+  ssh_key: "",
+  ssh_host_port: "7222",
+  ssh_guest_port: "22",
+  sudo_cmd: "echo '%p'|sudo -S sh \%f'",
+  shutdown_cmd: "shutdown -P now",
+  postinstall_files: [ "postinstall.sh", "postinstall_machinery.sh"],
+  postinstall_timeout: "10000",
+  hooks: {
+    # Before starting the build we spawn a webrick webserver which serves the
+    # autoyast profile to the installer. veewee"s built in webserver solution
+    # doesn"t work reliably with autoyast due to some timing issues.
+    before_create: Proc.new do
+      path = "#{Dir.pwd}/definitions/#{definition.box.name}"
+      Thread.new { WEBrick::HTTPServer.new(Port: 8888, DocumentRoot: path).start }
+    end
+  }
+)
---
+++ spec/definitions/veewee/definitions/machinery_opensuse13.2_kvm/postinstall_machinery.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# Add 13.2 repositories as they can not be added via
+# autoyast due to some bug.
+zypper --gpg-auto-import-keys ar --refresh --name "Main Repository (OSS)" http://download.opensuse.org/distribution/13.2/repo/oss/ download.opensuse.org-oss
+zypper --gpg-auto-import-keys ar --refresh --name "Main Repository (NON-OSS)" http://download.opensuse.org/distribution/13.2/repo/non-oss/ download.opensuse.org-non-oss
+
+zypper --gpg-auto-import-keys ar --refresh --name "Main Update Repository" http://download.opensuse.org/update/13.2/ download.opensuse.org-update
+zypper --gpg-auto-import-keys ar --refresh --name "Update Repository (Non-Oss)" http://download.opensuse.org/update/13.2-non-oss/ download.opensuse.org-13.2-non-oss
+
+zypper --gpg-auto-import-keys ar --disable --refresh --name "openSUSE-13.2-Debug" http://download.opensuse.org/debug/distribution/13.2/repo/oss/ repo-debug
+zypper --gpg-auto-import-keys ar --disable --refresh --name "openSUSE-13.2-Update-Debug" http://download.opensuse.org/debug/update/13.2/ repo-debug-update
+zypper --gpg-auto-import-keys ar --disable --refresh --name "openSUSE-13.2-Update-Debug-Non-Oss" http://download.opensuse.org/debug/update/13.2-non-oss/ repo-debug-update-non-oss
+
+zypper --gpg-auto-import-keys ar --disable --refresh --name "openSUSE-13.2-Source" http://download.opensuse.org/source/distribution/13.2/repo/oss/ repo-source
+
+zypper --non-interactive up
+
+# Install git for checking out the machinery code and other machinery dependencies
+zypper --non-interactive install --no-recommends git libxslt1 libxslt-devel zlib-devel libxml2-devel libvirt-devel expect patch
+
+# Install kiwi and dependencies for building
+zypper --non-interactive install --no-recommends kiwi kiwi-desc-vmxboot kiwi-tools db45-utils db-utils grub
+
+# Make sure that everything's written to disk, otherwise we sometimes get
+# empty files in the image
+sync
