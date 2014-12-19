9b9df42de9e68fec6496b7fe515c323babf566a4
Tim Hardeck <thardeck@suse.de>
2014-12-19 15:06:10 +0100

    Add external_medium? to Repository data model
    Kiwi- and Autoyast export both need this check so it does make sense
    to add it to the data model.

--- spec/unit/models/repositories_model_spec.rb
+++ spec/unit/models/repositories_model_spec.rb
@@ -27,3 +27,25 @@ describe "repositories model" do
 
   specify { expect(scope.first).to be_a(Repository) }
 end
+
+describe Repository do
+  subject { Repository.new }
+
+  describe "#external_medium?" do
+    it "returns true in case of a cd" do
+      subject.url = "cd:///?devices=/dev/disk/by-id/ata-QEMU_DVD-ROM_QM00001"
+      expect(subject.external_medium?).to be(true)
+    end
+
+    it "returns true in case of a cd" do
+      subject.url = "dvd:///?devices=/dev/disk/by-id/ata-QEMU_DVD-ROM_QM00001"
+      expect(subject.external_medium?).to be(true)
+    end
+
+    it "returns false in case of a network url" do
+      subject.url = "http://download.opensuse.org/distribution/13.1/repo/oss/"
+      expect(subject.external_medium?).to be(false)
+    end
+
+  end
+end
