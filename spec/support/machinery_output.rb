fcffa41029f76ac2a86d2a74a3cbfb0e7daf3c9a
Tim Hardeck <thardeck@suse.de>
2015-01-14 15:45:06 +0100

    Rename MachineryOutputSilencer to MachineryOutput
    The plan is to extend the MachineryOutput module to also capture the
    output in one variable so we can reuse the original Silencer module for
    it.
    
    To reflect this change the file and module is renamed.

--- spec/support/machinery_output.rb
+++ spec/support/machinery_output.rb
@@ -15,7 +15,7 @@
 # To contact SUSE about this file by physical or electronic mail,
 # you may find current contact information at www.suse.com
 
-module MachineryOutputSilencer
+module MachineryOutput
   def self.included(klass)
     klass.extend(self)
   end
