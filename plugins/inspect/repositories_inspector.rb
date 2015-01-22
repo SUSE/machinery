f1a80a73e4be94f0075db727504d9d4729f9d8ac
Cornelius Schumacher <cschum@suse.de>
2015-01-22 14:50:10 +0100

    Split up method to reduce its complexity

--- plugins/inspect/repositories_inspector.rb
+++ plugins/inspect/repositories_inspector.rb
@@ -36,79 +36,97 @@ class RepositoriesInspector < Inspector
       end
     end
 
-    if !details.empty?
-      # parse and remove header
-      idx_prio = details.first.index("Priority")
-      idx_alias = details.first.index("Alias")
-      details.shift
-
-      prio = {}
-      details.each_with_index do |entry,idx|
-        prio[entry[idx_alias]] = entry[idx_prio].to_i
-      end
-
-      credentials = {}
-      credential_dir = "/etc/zypp/credentials.d/"
-      credential_files = system.run_command(
-        "bash", "-c",
-        "test -d '#{credential_dir}' && ls -1 '#{credential_dir}' || echo ''",
-        :stdout => :capture
-      )
-      credential_files.split("\n").each do |f|
-        content = system.run_command(
-          "cat", "/etc/zypp/credentials.d/#{f}", :stdout => :capture
-        )
-        content.match(/username=(\w*)\npassword=(\w*)/)
-        credentials[f] = {
-          username: $1,
-          password: $2
-        }
-      end
-
-      reps = Nokogiri::XML(xml).xpath("/stream/repo-list/repo")
-      summary = "Found #{reps.count} repositories."
-      result = reps.map do |rep|
-        pri_value = rep["priority"] ? rep["priority"].to_i : prio[rep["alias"]]
-
-        # NCC
-        rep.at_xpath("./url").text.match(/\?credentials=(\w*)/)
-        cred_value = $1
-        if cred_value && credentials[cred_value]
-          username = credentials[cred_value][:username]
-          password = credentials[cred_value][:password]
-        end
-
-        # SCC
-        rep.at_xpath("./url").text.match(/(https:\/\/updates.suse.com\/SUSE\/)/)
-        scc_url = $1
-        cred_value = "SCCcredentials"
-        if scc_url && credentials[cred_value]
-          username = credentials[cred_value][:username]
-          password = credentials[cred_value][:password]
-        end
-
-        repository = Repository.new(
-          alias:       rep["alias"],
-          name:        rep["name"],
-          type:        rep["type"],
-          url:         rep.at_xpath("./url").text,
-          enabled:     rep["enabled"] == "1",
-          autorefresh: rep["autorefresh"] == "1",
-          gpgcheck:    rep["gpgcheck"] == "1",
-          priority:    pri_value
-        )
-        if username && password
-          repository[:username] = username
-          repository[:password] = password
-        end
-        repository
-      end.sort_by(&:name)
-    else
+    if details.empty?
       result = []
       summary = "Found 0 repositories."
+    else
+      priorities = parse_priorities_from_details(details)
+      credentials = get_credentials_from_system(system)
+      result = parse_repositories_from_xml(xml, priorities, credentials)
+      summary = "Found #{result.count} repositories."
     end
 
     description.repositories = RepositoriesScope.new(result)
     summary
+  end
+
+  def parse_priorities_from_details(details)
+    # parse and remove header
+    idx_prio = details.first.index("Priority")
+    idx_alias = details.first.index("Alias")
+    details.shift
+
+    prio = {}
+    details.each_with_index do |entry,idx|
+      prio[entry[idx_alias]] = entry[idx_prio].to_i
+    end
+
+    prio
+  end
+
+  def get_credentials_from_system(system)
+    credentials = {}
+    credential_dir = "/etc/zypp/credentials.d/"
+    credential_files = system.run_command(
+      "bash", "-c",
+      "test -d '#{credential_dir}' && ls -1 '#{credential_dir}' || echo ''",
+      :stdout => :capture
+    )
+    credential_files.split("\n").each do |f|
+      content = system.run_command(
+        "cat", "/etc/zypp/credentials.d/#{f}", :stdout => :capture
+      )
+      content.match(/username=(\w*)\npassword=(\w*)/)
+      credentials[f] = {
+        username: $1,
+        password: $2
+      }
+    end
+    credentials
+  end
+
+  def parse_repositories_from_xml(xml, priorities, credentials)
+    reps = Nokogiri::XML(xml).xpath("/stream/repo-list/repo")
+    result = reps.map do |rep|
+      if rep["priority"]
+        pri_value = rep["priority"].to_i
+      else
+        pri_value = priorities[rep["alias"]]
+      end
+
+      # NCC
+      rep.at_xpath("./url").text.match(/\?credentials=(\w*)/)
+      cred_value = $1
+      if cred_value && credentials[cred_value]
+        username = credentials[cred_value][:username]
+        password = credentials[cred_value][:password]
+      end
+
+      # SCC
+      rep.at_xpath("./url").text.match(/(https:\/\/updates.suse.com\/SUSE\/)/)
+      scc_url = $1
+      cred_value = "SCCcredentials"
+      if scc_url && credentials[cred_value]
+        username = credentials[cred_value][:username]
+        password = credentials[cred_value][:password]
+      end
+
+      repository = Repository.new(
+        alias:       rep["alias"],
+        name:        rep["name"],
+        type:        rep["type"],
+        url:         rep.at_xpath("./url").text,
+        enabled:     rep["enabled"] == "1",
+        autorefresh: rep["autorefresh"] == "1",
+        gpgcheck:    rep["gpgcheck"] == "1",
+        priority:    pri_value
+      )
+      if username && password
+        repository[:username] = username
+        repository[:password] = password
+      end
+      repository
+    end.sort_by(&:name)
+    result
   end
 end
