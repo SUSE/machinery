ae3301ae3f7fc1056038a5a7ad480f9f77cf2420
Tim Hardeck <thardeck@suse.de>
2015-01-22 20:29:35 +0100

    Support hard links in unmanaged-file inspector
    This should fix RHEL unmanaged-file inspection.

--- lib/tarball.rb
+++ lib/tarball.rb
@@ -33,6 +33,12 @@ class Tarball
           # This may fail for files with "->" in their name, but there is no way
           # how to avoid this when using "tar".
           path = rest.join(" ").split(" -> ").first
+        when "h"
+          type = :file
+          # This may fail for files with "link to" in their name, but there is no way
+          # how to avoid this when using "tar" unless we use the parameter
+          # --hard-dereference with tar to get rid of hard links
+          path = rest.join(" ").split(" link to ").first
         when "d"
           type = :dir
           path = rest.join(" ")[0..-2]   # Strip trailing "/".
