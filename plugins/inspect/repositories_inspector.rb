31a08429ebe593e280ecb05bef4ae256d27c39b1
Cornelius Schumacher <cschum@suse.de>
2015-01-22 14:33:24 +0100

    Move nokogiri requires to central file
    Our convention is to have all the requires in machinery.rb, so that
    the individual files don't need to require anything anymore.

--- plugins/inspect/repositories_inspector.rb
+++ plugins/inspect/repositories_inspector.rb
@@ -15,8 +15,6 @@
 # To contact SUSE about this file by physical or electronic mail,
 # you may find current contact information at www.suse.com
 
-require "nokogiri"
-
 class RepositoriesInspector < Inspector
   def inspect(system, description, options = {})
     system.check_requirement("zypper", "--version")
