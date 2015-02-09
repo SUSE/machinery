59c258f82817a154d87970ff13b07870880c3a2b
Tim Hardeck <thardeck@suse.de>
2015-02-09 18:41:52 +0100

    Add python helper script for yum repository extraction

---
+++ helpers/yum_repositories.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python
+# Copyright (c) 2013-2015 SUSE LLC
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of version 3 of the GNU General Public License as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.   See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, contact SUSE LLC.
+#
+# To contact SUSE about this file by physical or electronic mail,
+# you may find current contact information at www.suse.com
+
+import yum
+import json
+
+yb = yum.YumBase()
+
+repositories = []
+
+for repo in yb.repos.sort():
+  repo_dict = dict()
+  repo_dict["alias"] = repo.id
+  repo_dict["name"] = repo.name
+  repo_dict["type"] = "rpm-md"
+  repo_dict["url"] = repo.baseurl[0] or repo.getAttribute("metalink")
+  repo_dict["enabled"] = repo.enabled
+  repo_dict["gpgcheck"] = repo.gpgcheck
+  repo_dict["package_manager"] = "yum"
+  repositories.append(repo_dict)
+
+print(json.dumps(repositories))
