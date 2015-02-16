b083cd6cbd8b4c01dba2d226081dff9cd42ca26d
Tim Hardeck <thardeck@suse.de>
2015-02-16 15:35:05 +0100

    Improve listing system descriptions
    By using the SystemDescription.load option 'skip_validation'
    the listing time of 10 descriptions is improved by the factor
    10 on the developer machine.

--- lib/list_task.rb
+++ lib/list_task.rb
@@ -27,7 +27,7 @@ class ListTask
       descriptions.each do |name|
         name = File.basename(name)
         begin
-          description = SystemDescription.load(name, store)
+          description = SystemDescription.load(name, store, skip_validation: true)
         rescue Machinery::Errors::SystemDescriptionError
           Machinery::Ui.puts " #{name}:\n"
           Machinery::Ui.puts "   This description has an incompatible data format or is broken.\n" \
