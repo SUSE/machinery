79747457485a10d58d3a750148e087eff6b8566d
Andre Duffeck <aduffeck@suse.de>
2015-03-26 08:54:48 +0100

    Remove superfluous linebreaks

--- spec/unit/filter_spec.rb
+++ spec/unit/filter_spec.rb
@@ -22,18 +22,15 @@ describe Filter do
     it "parses array of definitions" do
       element_filter = Filter.parse_filter_definitions(["/foo=bar", "/baz=qux"])
       expect(element_filter.keys.length).to eq(2)
-      expect(element_filter["/foo"].matchers).
-        to eq(["bar"])
-      expect(element_filter["/baz"].matchers).
-        to eq(["qux"])
+      expect(element_filter["/foo"].matchers).to eq(["bar"])
+      expect(element_filter["/baz"].matchers).to eq(["qux"])
     end
 
     it "parses definition with multiple matcher" do
       element_filter = Filter.parse_filter_definitions("/foo=bar,baz")
       expect(element_filter.keys.length).to eq(1)
       expect(element_filter["/foo"].operator).to eq("=")
-      expect(element_filter["/foo"].matchers).
-        to eq([["bar", "baz"]])
+      expect(element_filter["/foo"].matchers).to eq([["bar", "baz"]])
     end
 
     it "parses definition with 'equals not' operator" do
@@ -46,8 +43,7 @@ describe Filter do
     it "handles escaped commas" do
       element_filter = Filter.parse_filter_definitions("/foo=bar,baz\\,qux")
       expect(element_filter.keys.length).to eq(1)
-      expect(element_filter["/foo"].matchers).
-        to eq([["bar", "baz,qux"]])
+      expect(element_filter["/foo"].matchers).to eq([["bar", "baz,qux"]])
     end
   end
 
