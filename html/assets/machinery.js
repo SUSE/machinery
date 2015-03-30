7219d65bf51873b6602bf1617056bda1dfe24d60
Andre Duffeck <aduffeck@suse.de>
2015-03-30 11:51:30 +0200

    Fix hound issues

--- html/assets/machinery.js
+++ html/assets/machinery.js
@@ -30,8 +30,9 @@ $(document).ready(function () {
   $.each(scopes, function(index, scope) {
     templates[scope] = Hogan.compile($("#scope_" + scope).html());
   });
-  templates["inspection_details_template"] = Hogan.compile($('#inspection_details_template').html());
-  template = Hogan.compile($('#content').html());
+  templates["inspection_details_template"] =
+    Hogan.compile($("#inspection_details_template").html());
+  template = Hogan.compile($("#content").html());
   $("#content_container").html(
     template.render(description, templates)
   );
@@ -72,7 +73,7 @@ $(document).ready(function () {
   $("#filter").keyup(function() {
     run_when_done_typing(function() {
       filterdocument();
-    }, 500)
+    }, 500);
   });
 
   clearFilter = function() {
@@ -86,35 +87,35 @@ $(document).ready(function () {
   $("a.scope_anchor").css("height", header_height);
   $("a.scope_anchor").css("margin-top", -header_height);
 
-  $('.scope_logo_big').each(function(){
+  $(".scope_logo_big").each(function(){
     var icon = $(this);
     $(window).scroll(function() {
-      icon.removeClass('fixed');
+      icon.removeClass("fixed");
       var pos = icon.offset();
       var top_pos = $(this).scrollTop() + header_height;
-      if(top_pos >= pos.top && icon.css('position') == 'static') {
-        icon.addClass('fixed').css("top", header_height);
-      } else if(top_pos <= pos.top && icon.hasClass('fixed')) {
-        icon.removeClass('fixed');
+      if(top_pos >= pos.top && icon.css("position") == "static") {
+        icon.addClass("fixed").css("top", header_height);
+      } else if(top_pos <= pos.top && icon.hasClass("fixed")) {
+        icon.removeClass("fixed");
       }
     })
   });
 
   // Hook up the toggle links
-  $('.toggle').click(function(){
-    $(this).closest('.scope').find('.scope_content').collapse('toggle');
+  $(".toggle").click(function(){
+    $(this).closest(".scope").find(".scope_content").collapse("toggle");
     $(this).toggleClass("collapsed");
   });
 
   $("#collapse-all").click(function(){
-    $(".scope_content").collapse('hide');
+    $(".scope_content").collapse("hide");
     $(".toggle").addClass("collapsed");
     $(this).hide();
     $("#expand-all").show();
   });
 
   $("#expand-all").click(function(){
-    $(".scope_content").collapse('show');
+    $(".scope_content").collapse("show");
     $(".toggle").removeClass("collapsed");
     $(this).hide();
     $("#collapse-all").show();
@@ -131,10 +132,10 @@ $(document).ready(function () {
   $(".diff-toggle").popover({
     trigger: "mouseenter",
     html: true,
-    template: '<div class="popover diff-popover" role="tooltip"><div class="arrow"></div><h3 class="popover-title"></h3><div class="popover-content"></div></div>',
+    template: "<div class='popover diff-popover' role='tooltip'><div class='arrow'></div><h3 class='popover-title'></h3><div class='popover-content'></div></div>",
     content: function() {
       file = $(this).data("config-file");
-      return $('*[data-config-file-diff="' + file + '"]').html();
+      return $("*[data-config-file-diff='" + file + "']").html();
     },
     title: function() {
       return "Changes for '" + $(this).data("config-file") + "'";
@@ -142,30 +143,30 @@ $(document).ready(function () {
   }).on("mouseenter",function () {
     clearTimeout(counter);
     var _this = this;
-    $('.diff-toggle').not(_this).popover('hide');
+    $(".diff-toggle").not(_this).popover("hide");
 
     counter = setTimeout(function(){
       $(_this).popover("show");
       $(".diff-popover").on("mouseleave", function () {
-          $('.diff-toggle').popover('hide');
+          $(".diff-toggle").popover("hide");
       });
     }, 100);
   }).on("mouseleave", function () {
     counter = setTimeout(function(){
       if (!$(".diff-popover:hover").length) {
-        $('.diff-toggle').popover('hide');
+        $(".diff-toggle").popover("hide");
       }
     }, 500);
   });
 
   // Set up inspection details popover
-  $('a.inspection_details').popover({
+  $("a.inspection_details").popover({
     trigger: "mouseenter",
     placement: "bottom",
     html: true,
-    template: '<div class="popover inspection-details-popover" role="tooltip"><div class="arrow"></div><h3 class="popover-title"></h3><div class="popover-content"></div></div>',
+    template: "<div class='popover inspection-details-popover' role='tooltip'><div class='arrow'></div><h3 class='popover-title'></h3><div class='popover-content'></div></div>",
     content: function() {
-      return $('#inspection_details').html();
+      return $("#inspection_details").html();
     },
     title: function() {
       return "Inspection details";
@@ -177,24 +178,24 @@ $(document).ready(function () {
     counter = setTimeout(function(){
       $(_this).popover("show");
       $(".inspection-details-popover").on("mouseleave", function () {
-          $('.inspection_details').popover('hide');
+          $(".inspection_details").popover("hide");
       });
     }, 100);
   }).on("mouseleave", function () {
     counter = setTimeout(function(){
       if (!$(".inspection-details-popover:hover").length) {
-        $('.inspection_details').popover('hide');
+        $(".inspection_details").popover("hide");
       }
     }, 500);
   });
 
   // Tooltips for service states
-  $("td.systemd_enabled, td.systemd_enabled-runtime").attr("title", 'Enabled through a symlink in .wants directory (permanently or just in /run).')
-  $("td.systemd_linked, td.systemd_linked-runtime").attr("title", 'Made available through a symlink to the unit file (permanently or just in /run).')
-  $("td.systemd_masked, td.systemd_masked-runtime").attr("title", 'Disabled entirely (permanently or just in /run).')
-  $("td.systemd_static").attr("title", 'Unit file is not enabled, and has no provisions for enabling in the "[Install]" section.')
-  $("td.systemd_indirect").attr("title", 'Unit file itself is not enabled, but it has a non-empty Also= setting in the "[Install]" section, listing other unit files that might be enabled.')
-  $("td.systemd_disabled").attr("title", 'Unit file is not enabled.')
-  $("td.sysvinit_on").attr("title", 'Service is enabled')
-  $("td.sysvinit_off").attr("title", 'Service is disabled')
+  $("td.systemd_enabled, td.systemd_enabled-runtime").attr("title", "Enabled through a symlink in .wants directory (permanently or just in /run).");
+  $("td.systemd_linked, td.systemd_linked-runtime").attr("title", "Made available through a symlink to the unit file (permanently or just in /run).");
+  $("td.systemd_masked, td.systemd_masked-runtime").attr("title", "Disabled entirely (permanently or just in /run).");
+  $("td.systemd_static").attr("title", "Unit file is not enabled, and has no provisions for enabling in the \"[Install]\" section.");
+  $("td.systemd_indirect").attr("title", "Unit file itself is not enabled, but it has a non-empty Also= setting in the \"[Install]\" section, listing other unit files that might be enabled.");
+  $("td.systemd_disabled").attr("title", "Unit file is not enabled.");
+  $("td.sysvinit_on").attr("title", "Service is enabled");
+  $("td.sysvinit_off").attr("title", "Service is disabled");
 });
