9bfe414e6d37850a8ba049f7e8df26d259709d7e
Tim Hardeck <thardeck@suse.de>
2015-04-29 17:21:17 +0200

    Add dedicated Tumbleweed integration test
    Since Tumbleweed will be updated regularly we can't use our regular
    integration tests with output matching.
    
    So we use for each inspector some regular expressions to make sure that
    they are still able to extract valid data from the system.

--- spec/integration/machinery_opensuse_tumbleweed_spec.rb
+++ spec/integration/machinery_opensuse_tumbleweed_spec.rb
@@ -18,6 +18,70 @@
 require_relative "integration_spec_helper"
 
 describe "machinery@openSUSE Tumbleweed" do
+  let(:test_data) {
+    {
+      "packages" =>
+        [
+          /\* zypper-\d+\..*\.x86_64 \(openSUSE\)$/,
+          /\* ca-certificates-\d.*\.noarch \(openSUSE\)$/
+        ],
+      "patterns" =>
+        [
+          /\* base$/
+        ],
+      "repositories" =>
+        [
+          /\* Main Repository \(OSS\)$/,
+          /URI: http:\/\/download\.opensuse\.org\/tumbleweed\/repo\/oss\/$/,
+          /Alias: download\.opensuse\.org-oss$/,
+          /Refresh: Yes$/,
+          /Priority: 99$/,
+          /Package Manager: zypp$/
+        ],
+      "services" =>
+        [
+          /\* sshd.service: enabled$/,
+          /\* rc-local.service: static$/,
+          /\* debug-shell.service: disabled$/
+        ],
+      "os" =>
+        [
+          /Name: openSUSE Tumbleweed$/,
+          /Version: .* \(Tumbleweed\)$/,
+          /Architecture: x86_64$/
+        ],
+      "users" =>
+        [
+          /\* \+ \(N\/A, uid: N\/A, gid: N\/A, shell: \)$/,
+          /\* bin \(bin, uid: 1, gid: 1, shell: \/bin\/bash\)$/
+        ],
+      "groups" =>
+        [
+          /\* \+ \(gid: N\/A\)$/,
+          /\* bin \(gid: 1, users: daemon\)$/
+        ],
+      "config-files" =>
+        [
+          /Files extracted: yes$/,
+          /\* \/etc\/auto\.master \(autofs-\d+.*, size, md5\)$/
+        ],
+      "changed-managed-files" =>
+        [
+          /Files extracted: yes$/,
+          /\* \/usr\/share\/info\/sed.info.gz \(size, md5\)$/,
+          /\* \/usr\/share\/man\/man1\/sed\.1\.gz \(deleted\)$/
+        ],
+      "unmanaged-files" =>
+        [
+          /Files extracted: yes$/,
+          /\* \/etc\/magicapp\.conf \(file\)$/,
+          /User\/Group: root:root$/,
+          /Mode: [0-7]{3}$/,
+          /Size: \d+/
+        ]
+    }
+  }
+
   let(:machinery_config) {
     {
       machinery_dir: "/home/vagrant/.machinery",
@@ -37,5 +101,35 @@ describe "machinery@openSUSE Tumbleweed" do
   include_examples "validate"
   include_examples "upgrade format"
   include_examples "generate html"
-  include_examples "inspect", ["opensuse_tumbleweed"]
+
+  describe "inspect openSUSE Tumbleweed system" do
+    before(:all) do
+      @subject_system = start_system(box: "opensuse_tumbleweed")
+      prepare_machinery_for_host(@machinery, @subject_system.ip, password: "vagrant")
+    end
+
+    Inspector.all_scopes.map { |i| i.gsub("_", "-") }.each do |scope|
+      describe "--scope=#{scope}" do
+        it "inspects #{scope}" do
+          measure("Inspect #{scope}") do
+            @machinery.run_command(
+              "#{machinery_command} inspect #{@subject_system.ip} --extract-files" \
+                " --scope=#{scope} --name=test",
+              as: "vagrant",
+              stdout: :capture
+            )
+          end
+
+          show_output = @machinery.run_command(
+            "#{machinery_command} show test --scope=#{scope}",
+            as: "vagrant",
+            stdout: :capture
+          )
+          test_data[scope].each do |regex|
+            expect(show_output).to match(regex)
+          end
+        end
+      end
+    end
+  end
 end
