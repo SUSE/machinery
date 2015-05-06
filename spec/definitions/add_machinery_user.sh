f44d91de60d8968c0501f9bc2eb4bca11044dfff
Tim Hardeck <thardeck@suse.de>
2015-05-06 19:33:44 +0200

    Add /bin/cat to /etc/sudoers
    Cat in Red Hat 5 is located in /bin/cat instead of /usr/bin/cat.

--- spec/definitions/add_machinery_user.sh
+++ spec/definitions/add_machinery_user.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 /usr/sbin/useradd -m machinery
 echo "machinery:linux" | /usr/sbin/chpasswd
-echo 'machinery ALL=(ALL) NOPASSWD: /usr/bin/find,/usr/bin/cat,/usr/bin/rsync,/bin/rpm -V *,/bin/tar --create *' >> /etc/sudoers
+echo 'machinery ALL=(ALL) NOPASSWD: /usr/bin/find,/usr/bin/cat,/bin/cat,/usr/bin/rsync,/bin/rpm -V *,/bin/tar --create *' >> /etc/sudoers
