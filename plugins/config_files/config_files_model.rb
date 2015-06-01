52c7475ccbbe6164bc8bd6b60625d6a6ea99412e
Andre Duffeck <aduffeck@suse.de>
2015-06-01 15:21:23 +0200

    Make use of the SystemFile data model class in the config files scope

--- plugins/config_files/config_files_model.rb
+++ plugins/config_files/config_files_model.rb
@@ -15,7 +15,7 @@
 # To contact SUSE about this file by physical or electronic mail,
 # you may find current contact information at www.suse.com
 
-class ConfigFile < Machinery::Object
+class ConfigFile < Machinery::SystemFile
 end
 
 class ConfigFileList < Machinery::Array
