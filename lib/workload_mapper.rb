9f817c33c9a8c368af3362c91fea61874f7781c9
mauromorales <mamorales@suse.de>
2015-08-12 13:59:10 +0200

    Add WorkloadMapper and WorkloadMapperDSL classes
    With the initial functionality for MariaDB workloads

---
+++ lib/workload_mapper.rb
@@ -0,0 +1,65 @@
+# Copyright (c) 2013-2015 SUSE LLC
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of version 3 of the GNU General Public License as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.   See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, contact SUSE LLC.
+#
+# To contact SUSE about this file by physical or electronic mail,
+# you may find current contact information at www.suse.com
+class WorkloadMapper
+  def write_compose_file(workloads, path)
+    compose_nodes = {}
+    workloads.each do |workload, config|
+      compose_nodes.merge!(compose_service(workload, config))
+    end
+    File.write(File.join(path, "docker-compose.yml"), compose_nodes.to_yaml)
+  end
+
+  def compose_service(workload, config)
+    name = config["service"]
+    service = {
+      name => compact(load_compose_template(workload))
+    }
+    fill_in_template(service[name], config["parameters"])
+    service
+  end
+
+  def load_compose_template(workload)
+    template = YAML::load(File.read(File.join(workload_mapper_path, "#{workload}.yml")))
+    template[workload]
+  end
+
+  def identify_workloads(system_description)
+    workloads = {}
+    Dir["#{File.expand_path(workload_mapper_path)}/*.rb"].each do |file_path|
+      mapper = WorkloadMapperDSL.new(system_description)
+      workload = mapper.check_clue(File.read(file_path))
+      workloads.merge!(workload.to_h)
+    end
+    workloads
+  end
+
+  private
+
+  def workload_mapper_path
+    File.join(Machinery::ROOT, "workload_mapper")
+  end
+
+  def compact(service)
+    service.each { |_, attr| attr.is_a?(Hash) && attr.reject! { |_, val| val.nil? } }
+  end
+
+  def fill_in_template(service, parameters)
+    service.each do |sk, sv|
+      sv.is_a?(Hash) && sv.each { |ki, kv| service[sk][ki] = parameters[kv.to_s] }
+    end
+  end
+end
---
+++ spec/unit/workload_mapper_dsl_spec.rb
@@ -0,0 +1,52 @@
+# Copyright (c) 2013-2015 SUSE LLC
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of version 3 of the GNU General Public License as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.   See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, contact SUSE LLC.
+#
+# To contact SUSE about this file by physical or electronic mail,
+# you may find current contact information at www.suse.com
+
+require_relative "spec_helper"
+
+describe WorkloadMapperDSL do
+  let(:system_description) { double }
+  subject { WorkloadMapperDSL.new(system_description) }
+
+  describe "#identify" do
+    context "when no service is specified" do
+      let(:clue) { "identify 'foo'" }
+      specify "name and service equaliy" do
+        subject.check_clue(clue)
+        expect(subject.name).to eq("foo")
+        expect(subject.service).to eq("foo")
+      end
+    end
+
+    context "when service is specified" do
+      let(:clue) { "identify 'foo', 'bar'" }
+      it "sets the name and the service" do
+        subject.check_clue(clue)
+        expect(subject.name).to include("foo")
+        expect(subject.service).to include("bar")
+      end
+    end
+  end
+
+  describe "#parameter" do
+    let(:clue) { "parameter 'port', '1234'" }
+    it "appends a parameter to the current workflow" do
+      subject.check_clue(clue)
+
+      expect(subject.parameters).to include("port" => "1234")
+    end
+  end
+end
