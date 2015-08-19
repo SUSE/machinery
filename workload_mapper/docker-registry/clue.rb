a0cc2e17097009638897033dd46ef2267368dca2
mauromorales <mamorales@suse.de>
2015-08-19 16:56:05 +0200

    Add docker-registry clue and compose template

---
+++ workload_mapper/docker-registry/clue.rb
@@ -0,0 +1,10 @@
+docker_registry_config = "/etc/registry/config.yml"
+if system.runs_service?("registry") && system.has_file?(docker_registry_config)
+  identify "docker-registry", "registry"
+  volume = "/etc/registry:/registry"
+  parameter "volumes", [volume]
+  used_port = /.*:(\d\d\d\d+)/.match(system.read_config(docker_registry_config, "addr")).
+    to_a.fetch(1, nil)
+  port = "#{used_port}:5000"
+  parameter "ports", [port]
+end
