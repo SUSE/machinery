e3d70e0e80edb23cc9539c18a3c3aff6f54a5cb2
mauromorales <mamorales@suse.de>
2015-08-25 17:17:21 +0200

    Add rails workload clue

---
+++ workload_mapper/rails/clue.rb
@@ -0,0 +1,17 @@
+if system.runs_service?("apache2") && system.has_file?("/usr/bin/rails")
+  apache_vhosts = system["unmanaged_files"].files.select { |file| /vhosts\.d/ =~ file.name }.map { |file| file.name }
+  apache_vhosts.each do |vhost|
+
+    rails_env = system.read_config(vhost, "PassengerAppEnv")
+    rails_public = system.read_config(vhost, "DocumentRoot")
+
+    if !rails_env.empty? && /public/.match(rails_public)
+      identify "rails", "web"
+      parameter "ports", ["3000:3000"]
+      parameter "links", ["db"]
+      rails_root = rails_public.gsub(/\/public/,"")
+      extract rails_root, "data"
+      break #for now we only handle the first rails app that was found
+    end
+  end
+end
