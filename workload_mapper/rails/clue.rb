b4cfda487154c9ef88c96fe5da966c741e6651d2
mauromorales <mamorales@suse.de>
2015-08-27 11:32:50 +0200

    Fix hound issues rails clue

--- workload_mapper/rails/clue.rb
+++ workload_mapper/rails/clue.rb
@@ -1,7 +1,6 @@
 if system.runs_service?("apache2") && system.has_file?("/usr/bin/rails")
-  apache_vhosts = system["unmanaged_files"].files.select { |file| /vhosts\.d/ =~ file.name }.map { |file| file.name }
+  apache_vhosts = system["unmanaged_files"].files.select { |f| /vhosts\.d/ =~ f.name }.map(&:name)
   apache_vhosts.each do |vhost|
-
     rails_env = system.read_config(vhost, "PassengerAppEnv")
     rails_public = system.read_config(vhost, "DocumentRoot")
 
@@ -9,9 +8,9 @@ if system.runs_service?("apache2") && system.has_file?("/usr/bin/rails")
       identify "rails", "web"
       parameter "ports", ["3000:3000"]
       parameter "links", ["db"]
-      rails_root = rails_public.gsub(/\/public/,"")
+      rails_root = rails_public.gsub(/\/public/, "")
       extract rails_root, "data"
-      break #for now we only handle the first rails app that was found
+      break # for now we only handle the first rails app that was found
     end
   end
 end
