82994ce48a24c5d3a87cd41cde8e37d8ce745abe
Kevin Eberle <keberle@suse.de>
2015-09-04 09:27:31 +0200

    Remove check for empty scopes from #assert_scopes of SystemDescription

--- lib/system_description.rb
+++ lib/system_description.rb
@@ -218,7 +218,7 @@ class SystemDescription < Machinery::Object
   end
 
   def assert_scopes(*scopes)
-    missing = scopes.select { |scope| !self[scope] || self[scope].empty? }
+    missing = scopes.select { |scope| !self[scope] }
 
     unless missing.empty?
       raise Machinery::Errors::SystemDescriptionError.new(
