689174c5fb7543cf43015686fe300fd0a544ce9d
Andre Duffeck <aduffeck@suse.de>
2015-10-28 15:35:00 +0100

    Move Inspector#run_script_with_progress to System

--- spec/unit/system_spec.rb
+++ spec/unit/system_spec.rb
@@ -164,6 +164,23 @@ describe System do
     end
   end
 
+  describe "#run_script_with_progress" do
+    it "calls the callback with the output" do
+      system = LocalSystem.new
+      expect(system).to receive(:run_script) do |_script, options|
+        options[:stdout].puts("output1")
+        options[:stdout].puts("output2")
+      end
+
+      callback_buffer = ""
+      system.run_script_with_progress("script") do |chunk|
+        callback_buffer << chunk
+      end
+
+      expect(callback_buffer).to eq("output1\noutput2\n")
+    end
+  end
+
   describe "#has_command" do
     it "returns true if the system has the command" do
       system = LocalSystem.new
