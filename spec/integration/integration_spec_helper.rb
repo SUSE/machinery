b0f93e4abe9aa5c9d8ecf8a5a8c6da562bb050fb
Tim Hardeck <thardeck@suse.de>
2015-11-05 13:34:46 +0100

    Add openSUSE Leap integration tests

--- spec/integration/integration_spec_helper.rb
+++ spec/integration/integration_spec_helper.rb
@@ -74,6 +74,7 @@ def boxes
   {
     "openSUSE_13_2:x86_64" => "opensuse132",
     "openSUSE_13_1:x86_64" => "opensuse131",
+    "leap:x86_64" => "opensuse_leap",
     "sles12:x86_64" => "sles12",
     "sles11:x86_64" => "sles11sp3",
     "rhel5" => "rhel5",
