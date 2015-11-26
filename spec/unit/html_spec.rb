b50552e0d34d46341c89e73f00d2e9d6a4ab6c58
Manuel Schnitzer <mschnitzer@suse.de>
2015-11-26 11:45:04 +0100

    Add new unit test for the Html class

---
+++ spec/unit/html_spec.rb
@@ -0,0 +1,27 @@
+# Copyright (c) 2013-2015 SUSE LLC
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of version 3 of the GNU General Public License as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.   See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, contact SUSE LLC.
+#
+# To contact SUSE about this file by physical or electronic mail,
+# you may find current contact information at www.suse.com
+
+require_relative "spec_helper"
+
+describe Html do
+  context ".run_server" do
+    it "raises an exception when an IP and the `public` flag is provided" do
+      expect { Html.run_server(nil, ip: "127.0.0.1", port: 5000, public: true) }.to \
+        raise_error(RuntimeError)
+    end
+  end
+end
