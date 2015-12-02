7756b49a6984f836369f12ace9562f14f206df92
Andre Duffeck <aduffeck@suse.de>
2015-12-02 14:32:24 +0100

    Fix integration test

--- spec/integration/support/validate_examples.rb
+++ spec/integration/support/validate_examples.rb
@@ -20,7 +20,7 @@ shared_examples "validate" do
     it "validates a system description against JSON schemas" do
       expected = <<EOF
 In scope repositories: The property #0 did not contain a required property of 'url'.
-In scope services: The property #4 (services/state) was not of a minimum string length of 1.
+In scope services: The property #4 (_elements/state) was not of a minimum string length of 1.
 EOF
       system_description_file = "spec/data/descriptions/validation-error/manifest.json"
       system_description_dir = File.dirname(system_description_file)
