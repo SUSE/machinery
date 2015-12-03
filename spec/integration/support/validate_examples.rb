1ddfb60ad46bda7f684deef3a28f7b179a7121df
Tim Hardeck <thardeck@suse.de>
2015-12-03 09:30:11 +0100

    Fix validation error message for repositories

--- spec/integration/support/validate_examples.rb
+++ spec/integration/support/validate_examples.rb
@@ -19,7 +19,7 @@ shared_examples "validate" do
   describe "validate" do
     it "validates a system description against JSON schemas" do
       expected = <<EOF
-In scope repositories: The property #0 did not contain a required property of 'url'.
+In scope repositories: The property (_attributes) of type Hash did not match any of the required schemas.
 In scope services: The property #4 (_elements/state) was not of a minimum string length of 1.
 EOF
       system_description_file = "spec/data/descriptions/validation-error/manifest.json"
