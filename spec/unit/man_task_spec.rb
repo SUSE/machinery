3444a89f465a59b192b41c96f2125dde8c38d3cb
Andre Duffeck <aduffeck@suse.de>
2016-02-02 10:16:17 +0100

    Add test for the man command with the HTML option

--- spec/unit/man_task_spec.rb
+++ spec/unit/man_task_spec.rb
@@ -19,19 +19,46 @@ require_relative "spec_helper"
 
 describe ManTask do
   describe "#man" do
-    it "validates the availability of man by calling the 'validate_existence_of_package method'" do
-      expect(LocalSystem).to receive(:validate_existence_of_package).with("man")
-      allow_any_instance_of(Kernel).to receive(:system).with("man", subject.man_path)
+    context "on the console" do
+      it "validates the availability of man by calling the 'validate_existence_of_package method'" do
+        expect(LocalSystem).to receive(:validate_existence_of_package).with("man")
+        allow_any_instance_of(Kernel).to receive(:system).with("man", anything)
 
-      subject.man
+        subject.man({})
+      end
+
+      it "calls the machinery man page" do
+        allow(LocalSystem).to receive(:validate_existence_of_package).with("man")
+        expect(subject).to receive("system").
+          with("man", anything)
+
+        subject.man({})
+      end
     end
 
-    it "calls the machinery man page" do
-      allow(LocalSystem).to receive(:validate_existence_of_package).with("man")
-      expect(subject).to receive("system").
-        with("man", subject.man_path)
+    context "in the browser" do
+      include GivenFilesystemSpecHelpers
+      use_given_filesystem
+      silence_machinery_output
 
-      subject.man
+      before(:each) do
+        stub_const("Machinery::ROOT", given_directory)
+      end
+
+      it "shows an error and exits if the documentation does not exist" do
+        expect(Html).to_not receive(:run_server)
+        expect(Machinery::Ui).to receive(:warn)
+
+        subject.man(html: true)
+      end
+
+      it "opens the documentation in the browser if it exists" do
+        FileUtils.mkdir_p(File.join(Machinery::ROOT, "manual", "site"))
+        expect(Html).to receive(:run_server).and_return(double(join: nil))
+
+
+        subject.man(html: true)
+      end
     end
   end
 end
