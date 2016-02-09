34b2dd9f9d897d552632650529b5c54f70195d2a
Mauro Morales <mamorales@suse.com>
2016-02-09 12:55:21 +0100

    Update migration 6to7 copyright date

--- spec/unit/migrations/migrate6to7_spec.rb
+++ spec/unit/migrations/migrate6to7_spec.rb
@@ -1,4 +1,4 @@
-# Copyright (c) 2013-2014 SUSE LLC
+# Copyright (c) 2013-2016 SUSE LLC
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of version 3 of the GNU General Public License as
