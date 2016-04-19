9e25fd0e4bf962a9b014fb0759c185b0c4b3c3ee
Mauro Morales <mamorales@suse.com>
2016-04-19 10:00:41 +0200

    Rename landing page for desc selector partial

--- spec/features/homepage_spec.rb
+++ spec/features/homepage_spec.rb
@@ -7,12 +7,36 @@ RSpec::Steps.steps "Showing the home/list page", type: :feature do
     )
   end
 
-  it "opens the landing page" do
+  it "opens the homepage" do
     visit("/")
     expect(page.status_code).to eq(200)
   end
 
   it "starts with the modal view to select a description open" do
     expect(page).to have_content("Select a description")
+  end
+
+  it "shows the table with all system descriptions" do
+    within("#content_container") do
+      find("th", text: "Last update")
+    end
+  end
+
+  it "shows the table with system description opensuse131" do
+    within("#content_container") do
+      find("td", text: "opensuse131")
+    end
+  end
+
+  it "filters the results" do
+    expect(page.all(".description-filterable tbody tr").size).to eq(3)
+
+    fill_in "descriptions-filter", with: "leap"
+
+    expect(page.all(".description-filterable tbody tr").size).to eq(1)
+
+    click_link("Reset")
+
+    expect(page.all(".description-filterable tbody tr").size).to eq(3)
   end
 end
