11bff3d5f3ac9302ee64db50bf5462ca6c32a92f
Mario Manno <mario.manno@suse.com>
2016-09-23 16:41:45 +0200

    Remove empty line

--- lib/static_html.rb
+++ lib/static_html.rb
@@ -16,7 +16,6 @@
 # To contact SUSE about this file by physical or electronic mail,
 # you may find current contact information at www.suse.com
 class StaticHtml < Exporter
-
   TEMPLATE_DIR = File.join(Machinery::ROOT, "html").freeze
   PARTIAL_DIR = File.join(Machinery::ROOT, "html", "partials").freeze
 
