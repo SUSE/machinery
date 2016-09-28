90f6d17e51e486154c0e60bed12c96908c2bbd39
Tim Hardeck <thardeck@suse.de>
2016-09-28 16:11:26 +0200

    Move mktmpdir to an around block in static html spec

--- spec/unit/static_html_spec.rb
+++ spec/unit/static_html_spec.rb
@@ -22,6 +22,13 @@ describe StaticHtml do
   capture_machinery_output
   initialize_system_description_factory_store
 
+  around(:each) do |test|
+    Dir.mktmpdir do |tmp_dir|
+      @tmp_dir = tmp_dir
+      test.run
+    end
+  end
+
   let(:description) {
     create_test_description(
       store_on_disk: true,
@@ -50,62 +57,52 @@ describe StaticHtml do
 
   describe "#write" do
     it "renders an HTML report" do
-      Dir.mktmpdir do |tmp_dir|
-        static_html = StaticHtml.new(description, tmp_dir)
-        static_html.write
-        index_file = File.join(tmp_dir, "index.html")
-        expect(File.readable?(index_file)).to be_truthy
-        expect(File.read(index_file)).to include("<html", "openSUSE")
-        expect(File.read(index_file)).not_to include(
-          "modal-footer",
-          "description-selector-label",
-          "open-description-selector"
-        )
-      end
+      static_html = StaticHtml.new(description, @tmp_dir)
+      static_html.write
+      index_file = File.join(@tmp_dir, "index.html")
+      expect(File.readable?(index_file)).to be_truthy
+      expect(File.read(index_file)).to include("<html", "openSUSE")
+      expect(File.read(index_file)).not_to include(
+        "modal-footer",
+        "description-selector-label",
+        "open-description-selector"
+      )
     end
 
     it "copies the assets over" do
-      Dir.mktmpdir do |tmp_dir|
-        StaticHtml.new(description, tmp_dir).write
-        expect(Dir.exist?(File.join(tmp_dir, "assets"))).to be(true)
-      end
+      StaticHtml.new(description, @tmp_dir).write
+      expect(Dir.exist?(File.join(@tmp_dir, "assets"))).to be(true)
     end
 
     it "does not copy the compare asset over" do
-      Dir.mktmpdir do |tmp_dir|
-        StaticHtml.new(description, tmp_dir).write
-        expect(Dir.exist?(File.join(tmp_dir, "assets", "compare"))).to be(false)
-      end
+      StaticHtml.new(description, @tmp_dir).write
+      expect(Dir.exist?(File.join(@tmp_dir, "assets", "compare"))).to be(false)
     end
   end
 
   describe "#create_directory" do
     it "raises an exception if dir exists and not forcing" do
-      Dir.mktmpdir do |tmp_dir|
-        static_html = StaticHtml.new(description, tmp_dir)
-        expect {
-          static_html.create_directory(false)
-        }.to raise_error(Machinery::Errors::ExportFailed)
-      end
-    end
-    it "removes directory and creates new one if forcing" do
-      Dir.mktmpdir do |tmp_dir|
-        static_html = StaticHtml.new(description, tmp_dir)
-        existing_file = File.join(tmp_dir, "test")
-        FileUtils.touch(existing_file)
-        expect {
-          static_html.create_directory(true)
-        }.not_to raise_error
-        expect(File.readable?(existing_file)).to be_falsy
-      end
-    end
-    it "creates a new directory" do
-      Dir.mktmpdir do |tmp_dir|
-        static_html = StaticHtml.new(description, tmp_dir)
-        Dir.rmdir(tmp_dir)
+      static_html = StaticHtml.new(description, @tmp_dir)
+      expect {
         static_html.create_directory(false)
-        expect(File.directory?(tmp_dir)).to be_truthy
-      end
+      }.to raise_error(Machinery::Errors::ExportFailed)
+    end
+
+    it "removes directory and creates new one if forcing" do
+      static_html = StaticHtml.new(description, @tmp_dir)
+      existing_file = File.join(@tmp_dir, "test")
+      FileUtils.touch(existing_file)
+      expect {
+        static_html.create_directory(true)
+      }.not_to raise_error
+      expect(File.readable?(existing_file)).to be_falsy
+    end
+
+    it "creates a new directory" do
+      static_html = StaticHtml.new(description, @tmp_dir)
+      Dir.rmdir(@tmp_dir)
+      static_html.create_directory(false)
+      expect(File.directory?(@tmp_dir)).to be_truthy
     end
   end
 end
